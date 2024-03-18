63ce6cf66abfa3ccb1892303479c6ab755b409c0
cyl <Cyl221002907@bjfu.edu.cn>
2024-03-18 18:50:32 +0800

    version2

--- src/main/java/com/git_test/gitTest.java
+++ src/main/java/com/git_test/gitTest.java
@@ -3,6 +3,6 @@ package com.git_test;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello gits!");
-        System.out.println("version2");
+        System.out.println("version3");
     }
 }
